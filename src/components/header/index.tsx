3830d8b997a9d669bb5a57dbb07307424b5ef5a5
Marcos V Silva <mrcsvs@outlook.com>
2025-07-25 21:38:32 -0300

    NextAuth + Google Provider

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import styles from "./styles.module.css";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function Header() {
+  const { data: session, status } = useSession();
+
   return (
     <header className={styles.header}>
       <section className={styles.content}>
@@ -11,12 +14,27 @@ export function Header() {
               Tarefas<span>+</span>
             </h1>
           </Link>
-          <Link className={styles.link} href={"/dashboard"}>
-            Meu Painel
-          </Link>
+          {session?.user && (
+            <Link className={styles.link} href={"/dashboard"}>
+              Meu Painel
+            </Link>
+          )}
         </nav>
 
-        <button className={styles.loginButton}>Acessar</button>
+        {status === "loading" ? (
+          <></>
+        ) : session ? (
+          <button className={styles.loginButton} onClick={() => signOut()}>
+            Olá {session.user?.name}
+          </button>
+        ) : (
+          <button
+            className={styles.loginButton}
+            onClick={() => signIn("google")}
+          >
+            Acessar
+          </button>
+        )}
       </section>
     </header>
   );
